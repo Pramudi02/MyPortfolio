794eb2142cf83e9f05213443dd81666aaac9fe34
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2025-05-17 21:37:46 +0530

    Updated contact form with improved styling, validation, and added personal assets for About page

--- src/app/pages/typing-text/typing-text.component.ts
+++ src/app/pages/typing-text/typing-text.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-typing-text',
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
   templateUrl: './typing-text.component.html',
   styleUrl: './typing-text.component.css'
 })
-export class TypingTextComponent {
 
+export class TypingTextComponent implements OnInit {
+
+  roles: string[] = [
+    'an Undergraduate...',
+    'a UI & UX Designer...',
+    'a Graphic Designer...',
+    'a Local Artist...'
+  ];
+
+  displayText: string = '';
+  currentRoleIndex = 0;
+  currentCharIndex = 0;
+  isDeleting = false;
+
+  ngOnInit(): void {
+    this.typeEffect();
+  }
+
+  typeEffect(): void {
+    const current = this.roles[this.currentRoleIndex];
+
+    if (this.isDeleting) {
+      this.displayText = current.substring(0, this.currentCharIndex--);
+    } else {
+      this.displayText = current.substring(0, this.currentCharIndex++);
+    }
+
+    let typingSpeed = this.isDeleting ? 50 : 100;
+
+    if (!this.isDeleting && this.currentCharIndex === current.length) {
+      typingSpeed = 1500;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.currentCharIndex === 0) {
+      this.isDeleting = false;
+      this.currentRoleIndex = (this.currentRoleIndex + 1) % this.roles.length;
+      typingSpeed = 500;
+    }
+
+    setTimeout(() => this.typeEffect(), typingSpeed);
+  }
 }
