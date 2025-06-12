c09a7b0aaceb593c8b7a70db3fdea84118c4bfd1
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2025-06-13 00:00:10 +0530

    Add education section to homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from '../about/about.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ContactComponent } from '../contact/contact.component';
+import { EducationComponent } from '../education/education.component';
 
 @Component({
     selector: 'app-homepage',
@@ -13,7 +14,8 @@ import { ContactComponent } from '../contact/contact.component';
         AboutComponent,
         SkillsComponent,
         ProjectsComponent,
-        ContactComponent
+        ContactComponent,
+        EducationComponent
     ],
     templateUrl: './homepage.component.html',
     styleUrl: './homepage.component.css'
