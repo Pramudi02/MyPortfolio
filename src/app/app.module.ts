0af10a7db7c87421d564397fb881fcf144224194
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2025-06-13 00:02:58 +0530

    Add AppModule to include EducationComponent

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { EducationComponent } from './pages/education/education.component';
+
+@NgModule({
+  imports: [
+    EducationComponent
+  ],
+})
+export class AppModule { } 
