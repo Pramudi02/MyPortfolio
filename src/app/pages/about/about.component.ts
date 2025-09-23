72e1ab615196ba93f693a029c75d608f497fa946
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2025-09-23 05:52:00 +0530

    Add typing text animation to about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TypingTextComponent } from '../typing-text/typing-text.component';
 
 @Component({
     selector: 'app-about',
     standalone: true,
-    imports: [],
+    imports: [TypingTextComponent],
     templateUrl: './about.component.html',
     styleUrl: './about.component.css'
 })
